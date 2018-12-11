562ed886a8cb00e427b1e527a5a7952b66eb67aa
anjanasv <45767025+anjanasv@users.noreply.github.com>
2018-12-11 21:53:04 +0530

    both files are modified

---
+++ variables.js
@@ -0,0 +1,21 @@
+//variables and data types
+var firstName='john';
+var age= 28;
+
+console.log(firstName + ' '+'age'+' '+'is'+' '+age);
+
+var job='teacher';
+
+console.log(firstName + ' '+'is'+' '+'working'+' '+'as'+' '+job+' '+'at age'+' '+age);
+var lastName=prompt("what is last name of john?")
+console.log(firstName+' '+lastName);//it is prompted for asking value
+
+var firstName1;//as no value is assigned it's value is undefined
+
+console.log(firstName1);
+
+var lastName1="Mark";
+alert(lastName1);
+
+
+firstName1= 'Jack';
