aef2e847bfbf7218f0516b34b5bfab9a4af3a163
anjanasv <45767025+anjanasv@users.noreply.github.com>
2018-12-11 22:05:19 +0530

    more data types are added

--- variables.js
+++ variables.js
@@ -7,7 +7,7 @@ console.log(firstName + ' '+'age'+' '+'is'+' '+age);
 var job='teacher';
 
 console.log(firstName + ' '+'is'+' '+'working'+' '+'as'+' '+job+' '+'at age'+' '+age);
-var lastName=prompt("what is last name of john?")
+var lastName=prompt("what is last name of john?")//if we didn't give any value, it will take it as null
 console.log(firstName+' '+lastName);//it is prompted for asking value
 
 var firstName1;//as no value is assigned it's value is undefined
@@ -18,4 +18,11 @@ var lastName1="Mark";
 alert(lastName1);
 
 
-firstName1= 'Jack';
+firstName1= 'Jack';//this is string data type
+
+var _3year= 3;// this numerical data type
+
+var ismarried;// this is boolean data type
+ismarried= true;
+
+console.log(firstName1+' '+'is'+' '+'married'+' '+ismarried)
