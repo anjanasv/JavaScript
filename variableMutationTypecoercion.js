632264c964143dbf23736753a2bc3afc2ee254ea
anjanasv <45767025+anjanasv@users.noreply.github.com>
2018-12-11 22:33:32 +0530

    vMTC is concept 2

---
+++ variableMutationTypecoercion.js
@@ -0,0 +1,11 @@
+//variable Coercion
+var firstName='john';
+var age= 26; //here JS automatically convert string data type into numerical data type, this is called Variable coercion.
+console.log(firstName+' '+age);
+
+//variable mutation
+
+firstName='Mark';
+age='28';
+
+console.log(firstName+' '+age);
